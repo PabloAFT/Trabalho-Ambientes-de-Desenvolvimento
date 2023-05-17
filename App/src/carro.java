020b70e7d7e9dfb75a0cb3fba69100135d9df475
isaackmendess <isaackmendes172@gmail.com>
2023-05-17 18:57:50 -0300

    Correção de ortografia

--- App/src/carro.java
+++ App/src/carro.java
@@ -1,4 +1,9 @@
 public class Carro {
+    public Carro(){
+        this.ligarMotor();
+        this.frear();
+
+    }
     int velocidade;
     public void ligarMotor() {
         System.out.print("Motor está sendo ligado");   
@@ -7,7 +12,7 @@ public class Carro {
         System.out.print("Motor está sendo desligado");
         
     }
-    public void freiar() {
+    public void frear() {
         velocidade = 0;
         System.out.println("O carro foi parado.");
     }
