2f2e97aa45bf2f6c8b817e57f40f0f7c1ea65d43
isaackmendess <isaackmendes172@gmail.com>
2023-05-17 19:05:15 -0300

    adicionei os gets e sets

--- App/src/carro.java
+++ App/src/carro.java
@@ -4,10 +4,17 @@ public class Carro {
         this.frear();
 
     }
-    int velocidade;
+    private int velocidade;
     public void ligarMotor() {
         System.out.print("Motor está sendo ligado");   
     }
+    public int getVelocidade() {
+        return velocidade;
+    }
+    public void setVelocidade(int velocidade) {
+        this.velocidade=velocidade;
+    }
+    
     public void desligarMotor() {
         System.out.print("Motor está sendo desligado");
         
