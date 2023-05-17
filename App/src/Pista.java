7c96725dbf60aac25f925bec6b90c68022c2ada7
PabloAFT <pafiltorres@gmail.com>
2023-05-17 19:28:15 -0300

    Adicionado método chuva

--- App/src/Pista.java
+++ App/src/Pista.java
@@ -1,6 +1,16 @@
 public class Pista {
     private int distancia;
-
+    private boolean pistaMolhada;
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+    public int getDistancia() {
+        return distancia;
+    }
+    public void chuva() {
+        System.out.println("Chuva molha a pista, tornando-a escorregadia.");
+        this.pistaMolhada = true;
+    }
     public void simularCorrida(Carro carro) {
 
         System.out.println("O carro percorreu a pista em "+ (carro.getVelocidade() - this.distancia ));    
