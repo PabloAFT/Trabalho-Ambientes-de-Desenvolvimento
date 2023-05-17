bb8ef0c24bb132624ddd1b9511038c253fbb1e70
PabloAFT <pafiltorres@gmail.com>
2023-05-17 19:51:24 -0300

    Modificado método simularCorrida

--- App/src/Pista.java
+++ App/src/Pista.java
@@ -2,6 +2,11 @@ public class Pista {
     private int distancia;
     private boolean pistaMolhada;
     private int voltas;
+    public Pista(int distancia,int voltas){
+        this.distancia = distancia;
+        this.voltas = voltas;
+
+    }
     public void setDistancia(int distancia) {
         this.distancia = distancia;
     }
@@ -13,8 +18,11 @@ public class Pista {
         this.pistaMolhada = true;
     }
     public void simularCorrida(Carro carro) {
+        for (int i = 0; i < voltas; i++) {
+            percorrerVolta(carro);
+        }
+        System.out.println("O carro percorreu a pista em "+ (carro.getVelocidade() / this.distancia*this.voltas ));
 
-        System.out.println("O carro percorreu a pista em "+ (carro.getVelocidade() / this.distancia ));
     
     }
     public void percorrerVolta(Carro carro){
