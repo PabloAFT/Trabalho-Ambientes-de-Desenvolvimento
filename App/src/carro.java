791c11676fd4f0ee26eb8aeb053018f32bee891d
isaackmendess <isaackmendes172@gmail.com>
2023-05-17 18:45:42 -0300

    Adicionado classe carro

---
+++ App/src/carro.java
@@ -0,0 +1,10 @@
+public class carro {
+    int velocidade;
+    public void ligarMotor() {
+        System.out.print("Motor está sendo ligado");   
+    }
+    public void desligarMotor() {
+        System.out.print("Motor está sendo desligado");
+        
+    }
+}
